25e9cc1706c018298c50f4a3e398c052c2e38e3b
Rizwana11kadher <rizwanabegum.1523bme@gmail.com>
2024-05-21 12:20:20 -0700

    BRAND NAME GENERATOR.py

--- project1.py
+++ project1.py
@@ -12,3 +12,10 @@ print("Welcome to Brand Name Genertor!")
 city=input("in which city you grew up ?")
 pet=input("what  is the name of the pet ?")
 print("Your brand name could be "+city+" "+pet+"\n")
+
+#OUTPUT:
+##Welcome to Brand Name Genertor!
+#in which city you grew up ?CHENNAI
+#what  is the name of the pet ?CAT
+#Your brand name could be CHENNAI CAT
+#AND THE CURSOR WILL SHOW ON THE NEXT LINE BECAUSE WE HAVE CONCATENATE THE NEW LINE[\n]  ATLAST  IN STRING FORM INSIDE PRINT FUNCTON
